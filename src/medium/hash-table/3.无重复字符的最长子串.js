d73069a72a19f77c9332a81e1db650396c518051
kelekexiao123 <250407778@qq.com>
2020-02-18 16:53:35 +0800

    :sparkles: AC: 3 并更新题解

---
+++ src/medium/hash-table/3.无重复字符的最长子串.js
@@ -0,0 +1,54 @@
+// # [3] 无重复字符的最长子串
+
+// >给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+// >
+// >>示例 1:
+// >>
+// >>输入: "abcabcbb"
+// >>
+// >>输出: 3
+// >>
+// >>解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// >>
+// >>示例 2:
+// >>
+// >>输入: "bbbbb"
+// >>
+// >>输出: 1
+// >>
+// >>解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// >>
+// >>示例 3:
+// >>
+// >>输入: "pwwkew"
+// >>
+// >>输出: 3
+// >>
+// >>解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+// >
+// >请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  const len = s.length;
+  let res = 0;
+  let hash = {};
+  let start = 0;
+  for (let i = 0; i < len; i++) {
+    if (hash[s[i]] !== undefined && hash[s[i]] >= start) {
+      start = hash[s[i]] + 1;
+    }
+    hash[s[i]] = i;
+    if (i - start + 1 > res) {
+      res = i - start + 1;
+    }
+  }
+  return res;
+};
+// @lc code=end
+
+console.log(lengthOfLongestSubstring('pwwkew'));
