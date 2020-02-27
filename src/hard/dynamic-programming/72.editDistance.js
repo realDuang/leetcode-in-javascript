f3faa8cc5966ced97530c1b31d52c173aa1fa0e5
kelekexiao123 <250407778@qq.com>
2020-02-27 12:27:47 +0800

    :memo: 完善第70,72,75题解析

--- src/hard/dynamic-programming/72.editDistance.js
+++ src/hard/dynamic-programming/72.editDistance.js
@@ -42,4 +42,27 @@ var minDistance = function(word1, word2) {
 // 添加，子问题即word1.slice(0, i)转变为word2.slice(0, j-1)
 // 另外需要注意的是边界值的问题，为字符串与空字符的比较，因此第一横行与第一竖行的值从0递增，即不断执行添加操作。
 
+// minDistance = function(word1, word2) {
+//   const len1 = word1.length;
+//   const len2 = word2.length;
+
+//   const dp = new Array(len1 + 1).fill(0).map(x => new Array(len2 + 1).fill(0));
+//   for (let i = 0; i <= len2; i++) {
+//     dp[0][i] = i;
+//   }
+//   for (let i = 0; i <= len1; i++) {
+//     dp[i][0] = i;
+//   }
+//   for (let i = 1; i <= len1; i++) {
+//     for (let j = 1; j <= len2; j++) {
+//       if (word1[i - 1] === word2[j - 1]) {
+//         dp[i][j] = dp[i - 1][j - 1];
+//       } else {
+//         dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1;
+//       }
+//     }
+//   }
+//   return dp[len1][len2];
+// };
+
 console.log(minDistance('ros', 'horse'));
