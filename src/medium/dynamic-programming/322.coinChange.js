10e1bda391fbb48967aa3df1176aaf37cd0773fc
kelekexiao123 <250407778@qq.com>
2020-03-09 01:00:02 +0800

    :memo: 更新322的题解

--- src/medium/dynamic-programming/322.coinChange.js
+++ src/medium/dynamic-programming/322.coinChange.js
@@ -54,4 +54,59 @@ var coinChange = function(coins, amount) {
 // 我们也可以用一维表代替二维表，从而节省空间复杂度。
 // 状态转移方程：对每一种硬币coin：dp[i] = min(dp[i], dp[i - coin] + 1)
 
-console.log(coinChange([2, 5, 7], 11));
+// 贪心回溯解法
+// let res = Number.MAX_SAFE_INTEGER;
+// var coinChange = function(coins, amount) {
+//   if (amount === 0) return 0;
+//   // 从大到小排序
+//   coins.sort((a, b) => b - a);
+
+//   dfs(coins, 0, amount, 0);
+//   return res < Number.MAX_SAFE_INTEGER ? res : -1;
+// };
+
+// function dfs(coins, index, amount, count) {
+//   if (index >= coins.length) return;
+
+//   // 当剩余值为0,说明找到了一组最优解
+//   if (amount === 0) {
+//     res = Math.min(count, res);
+//     return;
+//   }
+
+//   for (let i = Math.floor(amount / coins[index]); i >= 0; i--) {
+//     const newCount = count + i;
+//     const rest = amount - i * coins[index];
+
+//     // 当前硬币数已经超过最优解了，剪枝
+//     if (newCount >= res) break;
+
+//     dfs(coins, index + 1, rest, newCount);
+//   }
+// }
+var coinChange = function(coins, amount) {
+  // 初始化dp二维数组与第一行、第一列的值
+  const dp = new Array(coins.length).fill(0).map(x => new Array(amount + 1).fill(amount + 1));
+  // amount=0时，硬币个数总为0
+  for (let i = 0; i < dp.length; i++) {
+    dp[i][0] = 0;
+  }
+  for (let i = 0; i < dp[0].length; i++) {
+    if (i % coins[0] === 0) dp[0][i] = Math.floor(i / coins[0]);
+  }
+
+  for (let j = 1; j <= amount; j++) {
+    for (let i = 1; i < coins.length; i++) {
+      if (j >= coins[i]) {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i]] + 1);
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+  console.log(dp);
+  return dp[coins.length - 1][amount] < amount + 1 ? dp[coins.length - 1][amount] : -1;
+};
+
+console.log(coinChange([7, 1, 10], 14));
+// console.log(coinChange([6, 4, 10], 18));
