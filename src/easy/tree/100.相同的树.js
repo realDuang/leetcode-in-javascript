9ba86b55ecc3af675878c6dc21a7c37df2547484
kelekexiao123 <250407778@qq.com>
2020-04-03 11:59:19 +0800

    :sparkles: AC:98,100,101

---
+++ src/easy/tree/100.相同的树.js
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=javascript
+ *
+ * [100] 相同的树
+ *
+ * https://leetcode-cn.com/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (56.80%)
+ * Likes:    337
+ * Dislikes: 0
+ * Total Accepted:    76K
+ * Total Submissions: 133.4K
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   3     2   3
+ *
+ * ⁠       [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入:      1          1
+ * ⁠         /           \
+ * ⁠        2             2
+ *
+ * ⁠       [1,2],     [1,null,2]
+ *
+ * 输出: false
+ *
+ *
+ * 示例 3:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   1     1   2
+ *
+ * ⁠       [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (!p && !q) return true;
+  if ((p && !q) || (!p && q)) return false;
+  if (p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+// @lc code=end
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+const a = new TreeNode(1);
+const b = new TreeNode(2);
+const c = new TreeNode(3);
+const d = new TreeNode(4);
+
+const e = new TreeNode(1);
+const f = new TreeNode(2);
+const g = new TreeNode(3);
+const h = new TreeNode(4);
+
+a.left = b;
+a.right = c;
+b.left = d;
+
+e.left = f;
+e.right = g;
+f.left = h;
+
+console.log(isSameTree(a, e));
