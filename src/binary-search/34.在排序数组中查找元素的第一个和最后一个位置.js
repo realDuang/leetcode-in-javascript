0283597ba2c3c3179ca16a18755a0162d741551d
kelekexiao123 <250407778@qq.com>
2020-05-07 15:36:51 +0800

    :sparkles: AC: 34

---
+++ src/binary-search/34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=javascript
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (39.24%)
+ * Likes:    416
+ * Dislikes: 0
+ * Total Accepted:    87.8K
+ * Total Submissions: 222.4K
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 如果数组中不存在目标值，返回 [-1, -1]。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: [3,4]
+ *
+ * 示例 2:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: [-1,-1]
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  const res = [-1, -1];
+  if (nums.length === 0) return res;
+
+  // 二分查左侧边界
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] >= target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  if (left < nums.length && nums[left] === target) {
+    res[0] = left;
+  }
+
+  // 二分查右侧边界
+  left = 0;
+  right = nums.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] <= target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  if (right >= 0 && nums[right] === target) {
+    res[1] = right;
+  }
+
+  return res;
+};
+// @lc code=end
+
+console.log(searchRange([5, 7, 7, 8, 8, 8, 8, 10], 8));
