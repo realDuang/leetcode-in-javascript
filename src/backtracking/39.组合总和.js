ae649adb819282e2d54e90d908fe44dafb48b2ae
kelekexiao123 <250407778@qq.com>
2020-08-09 17:12:55 +0800

    feat: :sparkles: AC:39

---
+++ src/backtracking/39.组合总和.js
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=39 lang=javascript
+ *
+ * [39] 组合总和
+ *
+ * https://leetcode-cn.com/problems/combination-sum/description/
+ *
+ * algorithms
+ * Medium (68.59%)
+ * Likes:    795
+ * Dislikes: 0
+ * Total Accepted:    120K
+ * Total Submissions: 172.7K
+ * Testcase Example:  '[2,3,6,7]\n7'
+ *
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ *
+ * candidates 中的数字可以无限制重复被选取。
+ *
+ * 说明：
+ *
+ *
+ * 所有数字（包括 target）都是正整数。
+ * 解集不能包含重复的组合。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：candidates = [2,3,6,7], target = 7,
+ * 所求解集为：
+ * [
+ * ⁠ [7],
+ * ⁠ [2,2,3]
+ * ]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：candidates = [2,3,5], target = 8,
+ * 所求解集为：
+ * [
+ * [2,2,2,2],
+ * [2,3,3],
+ * [3,5]
+ * ]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= candidates.length <= 30
+ * 1 <= candidates[i] <= 200
+ * candidate 中的每个元素都是独一无二的。
+ * 1 <= target <= 500
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+  const res = [];
+  candidates.sort((a, b) => a - b);
+  backtrack(0, target, []);
+  return res;
+
+  function backtrack(index, restTarget, track) {
+    if (restTarget === 0) {
+      res.push(track);
+      return;
+    }
+    for (let i = index; i < candidates.length; i++) {
+      if (candidates[i] > restTarget) break;
+      backtrack(i, restTarget - candidates[i], [...track, candidates[i]]);
+    }
+  }
+};
+
+// @lc code=end
+
+console.log(combinationSum([3, 12, 9, 11, 6, 7, 8, 5, 4], 15));
