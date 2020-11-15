d478b311a6586b3991e8b7e8ff9910cbba2ea6ba
realDuang <250407778@qq.com>
2020-11-15 16:51:05 +0800

    feat: :sparkles: AC: 538、1038

---
+++ src/tree/538.把二叉搜索树转换为累加树.js
@@ -0,0 +1,131 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=javascript
+ *
+ * [538] 把二叉搜索树转换为累加树
+ *
+ * https://leetcode-cn.com/problems/convert-bst-to-greater-tree/description/
+ *
+ * algorithms
+ * Medium (63.96%)
+ * Likes:    429
+ * Dislikes: 0
+ * Total Accepted:    66.8K
+ * Total Submissions: 104.5K
+ * Testcase Example:  '[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]'
+ *
+ * 给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），使每个节点 node
+ * 的新值等于原树中大于或等于 node.val 的值之和。
+ *
+ * 提醒一下，二叉搜索树满足下列约束条件：
+ *
+ *
+ * 节点的左子树仅包含键 小于 节点键的节点。
+ * 节点的右子树仅包含键 大于 节点键的节点。
+ * 左右子树也必须是二叉搜索树。
+ *
+ *
+ * 注意：本题和 1038:
+ * https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/
+ * 相同
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+ * 输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：root = [0,null,1]
+ * 输出：[1,null,1]
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：root = [1,0,2]
+ * 输出：[3,3,2]
+ *
+ *
+ * 示例 4：
+ *
+ * 输入：root = [3,2,4,1]
+ * 输出：[7,9,4,10]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中的节点数介于 0 和 10^4^ 之间。
+ * 每个节点的值介于 -10^4 和 10^4 之间。
+ * 树中的所有值 互不相同 。
+ * 给定的树为二叉搜索树。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function(root) {
+  let sum = 0;
+  function traverse(root) {
+    if (!root) return;
+
+    traverse(root.right);
+
+    sum += root.val;
+    root.val = sum;
+
+    traverse(root.left);
+  }
+
+  traverse(root);
+  return root;
+};
+// @lc code=end
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+var deserialize = function(arr) {
+  const root = new TreeNode(arr[0]);
+  const queue = [root];
+  for (let i = 1; i < arr.length; i += 2) {
+    const node = queue.shift();
+
+    const leftNumber = arr[i];
+    if (leftNumber !== 'null') {
+      node.left = new TreeNode(leftNumber);
+      queue.push(node.left);
+    }
+
+    const rightNumber = arr[i + 1];
+    if (i + 1 < arr.length && rightNumber !== 'null') {
+      node.right = new TreeNode(rightNumber);
+      queue.push(node.right);
+    }
+  }
+  return root;
+};
+
+const root = deserialize([4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8]);
+console.log(convertBST(root));
