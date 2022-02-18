b781854da3f7e1f50d161c51e9b8cd9a0320f7e8
realDuang <250407778@qq.com>
2022-02-18 17:17:24 +0800

    feat: AC:22 并更新题解

---
+++ src/backtracking/22.括号生成.ts
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=typescript
+ *
+ * [22] 括号生成
+ *
+ * https://leetcode-cn.com/problems/generate-parentheses/description/
+ *
+ * algorithms
+ * Medium (77.36%)
+ * Likes:    2367
+ * Dislikes: 0
+ * Total Accepted:    425K
+ * Total Submissions: 549.3K
+ * Testcase Example:  '3'
+ *
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 1
+ * 输出：["()"]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= n <= 8
+ *
+ *
+ */
+
+// @lc code=start
+function generateParenthesis(n: number): string[] {
+  const res: string[] = [];
+  backtrack('', 0, 0);
+  return res;
+
+  function backtrack(str: string, left: number, right: number) {
+    if (left === n && right === n) {
+      res.push(str);
+    }
+    left < n && backtrack(str + '(', left + 1, right);
+    right < left && backtrack(str + ')', left, right + 1);
+  }
+}
+// @lc code=end
+
+(() => {
+  const n = 3;
+  console.log(generateParenthesis(n));
+})();
