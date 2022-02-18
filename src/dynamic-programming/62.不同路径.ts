ea5eba617b9a744bb872e311db2a63bcd80e7890
realDuang <250407778@qq.com>
2022-02-18 16:10:12 +0800

    feat: AC:62,63 并更新题解

---
+++ src/dynamic-programming/62.不同路径.ts
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=typescript
+ *
+ * [62] 不同路径
+ *
+ * https://leetcode-cn.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (66.73%)
+ * Likes:    1279
+ * Dislikes: 0
+ * Total Accepted:    370.9K
+ * Total Submissions: 555.8K
+ * Testcase Example:  '3\n7'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ *
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ *
+ * 问总共有多少条不同的路径？
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：m = 3, n = 7
+ * 输出：28
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：m = 3, n = 2
+ * 输出：3
+ * 解释：
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向下
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：m = 7, n = 3
+ * 输出：28
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：m = 3, n = 3
+ * 输出：6
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * 题目数据保证答案小于等于 2 * 10^9
+ *
+ *
+ */
+
+// @lc code=start
+function uniquePaths(m: number, n: number): number {
+  // dp[i][j] 表示到达该处不同的路径条数
+  // 状态转移方程：dp[i][j] = dp[i-1][j] + dp[i][j-1]
+  const dp = Array(m)
+    .fill(0)
+    .map(x => Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let i = 0; i < n; i++) {
+    dp[0][i] = 1;
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+  return dp[m - 1][n - 1];
+}
+// @lc code=end
+
+(() => {
+  const m = 3,
+    n = 7;
+  console.log(uniquePaths(m, n));
+})();
