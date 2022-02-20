54990831a475925cac00f750920a768bcde5daee
realDuang <250407778@qq.com>
2022-02-20 20:55:05 +0800

    feat: AC:91 并更新题解

--- src/dynamic-programming/91.解码方法.js
+++ src/dynamic-programming/91.解码方法.js
@@ -65,4 +65,4 @@ var numDecodings = function(s) {
 
 // 状态转移方程： dp[n] = (1<=single<=9: dp[n - 1]) + (10<=double<=26: dp[n - 2])
 // 动态规划问题，从后往前倒退，若最后一个数字为1-9，那么它的情况与dp[n-1]的情况一样多，排列为每种情况在最后面加上最后一个数字即可，同理，若最后两个数字为10-26，它的情况与dp[n-2]一样多，最后的结果即为两者相加的和。
-console.log(numDecodings('172'));
+console.log(numDecodings('10'));
