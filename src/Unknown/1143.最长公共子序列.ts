c58df1474bd611f2911b820d4289561992ac3490
realDuang <250407778@qq.com>
2022-02-21 11:56:08 +0800

    feat: AC:1143 并更新题解

---
+++ src/Unknown/1143.最长公共子序列.ts
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=typescript
+ *
+ * [1143] 最长公共子序列
+ *
+ * https://leetcode-cn.com/problems/longest-common-subsequence/description/
+ *
+ * algorithms
+ * Medium (64.01%)
+ * Likes:    840
+ * Dislikes: 0
+ * Total Accepted:    196.7K
+ * Total Submissions: 307.3K
+ * Testcase Example:  '"abcde"\n"ace"'
+ *
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ *
+ * 一个字符串的 子序列
+ * 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ *
+ *
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ *
+ *
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：text1 = "abcde", text2 = "ace"
+ * 输出：3
+ * 解释：最长公共子序列是 "ace" ，它的长度为 3 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc" ，它的长度为 3 。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：text1 = "abc", text2 = "def"
+ * 输出：0
+ * 解释：两个字符串没有公共子序列，返回 0 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * text1 和 text2 仅由小写英文字符组成。
+ *
+ *
+ */
+
+// @lc code=start
+function longestCommonSubsequence(text1: string, text2: string): number {
+  const len1 = text1.length;
+  const len2 = text2.length;
+  const dp = Array(len1 + 1)
+    .fill(0)
+    .map(x => Array(len2 + 1).fill(0));
+
+  for (let i = 1; i <= text1.length; i++) {
+    for (let j = 1; j <= text2.length; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[len1][len2];
+}
+// @lc code=end
+
+(() => {
+  const text1 = 'abcde',
+    text2 = 'ace';
+  console.log(longestCommonSubsequence(text1, text2));
+})();
