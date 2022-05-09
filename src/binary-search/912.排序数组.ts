bb2337c447744f6f91148e1c0ee3f102225bca6c
realDuang <250407778@qq.com>
2022-05-10 00:42:40 +0800

    feat: AC:912 并更新题解

--- src/binary-search/912.排序数组.ts
+++ src/binary-search/912.排序数组.ts
@@ -51,46 +51,95 @@ function sortArray(nums: number[]): number[] {
   return nums;
 
   function sort(left: number, right: number) {
-    if (left === right) {
-      // 单个元素不用排序
+    if (left >= right) {
       return;
     }
-    const mid = Math.floor((left + right) / 2);
-    sort(left, mid);
-    sort(mid + 1, right);
-    merge(left, mid, right);
+    // 对 [left, right] 进行一次快排
+    // 使得 nums[left, index-1] <= nums[index] < nums[p+1, right]
+    const index = partition(left, right);
+    // 之后对左右两侧的数组递归进行快排
+    sort(left, index - 1);
+    sort(index + 1, right);
   }
 
-  function merge(left: number, mid: number, right: number) {
-    const temp = [];
-    let l = left;
-    let r = mid + 1;
-    while (l < mid + 1 || r < right + 1) {
-      if (l === mid + 1) {
-        // 左侧数组已经全部合并，则将右边剩余数组直接拼接下来
-        temp.push(nums[r]);
-        r += 1;
-      } else if (r === right + 1) {
-        // 右侧数组已经全部合并，则将左边剩余数组直接拼接下来
-        temp.push(nums[l]);
-        l += 1;
-      } else if (nums[l] > nums[r]) {
-        // 左右指针两数相比取较小值，并前进指针
-        temp.push(nums[r]);
-        r += 1;
-      } else {
-        // temp[l] <= temp[r]
-        temp.push(nums[l]);
+  function partition(left: number, right: number) {
+    let flag = nums[left];
+    let l = left + 1;
+    let r = right;
+    while (l <= r) {
+      while (l < right && nums[l] <= flag) {
         l += 1;
       }
+      while (r > left && nums[r] > flag) {
+        r -= 1;
+      }
+      // 此时 [left, l) 所有元素都小于 flag，(r, right] 所有元素都大于 flag
+      if (l >= r) {
+        break;
+      }
+      // 此时存在 nums[l] > flag, nums[r] <= flag，因此将两者数值进行交换
+      // 使得[left, l+1) 所有元素都小于 flag，(r-1, right] 所有元素都大于 flag
+      swap(l, r);
     }
-    // 原地更新数组
-    for (let i = left; i <= right; i++) {
-      nums[i] = temp[i - left];
-    }
+    // 此时l = r + 1。r 的左侧元素都小于 flag，右侧元素都大于 flag
+    // 因此，r 的位置为 flag 排序后应当在的位置，做一次交换
+    swap(left, r);
+    // 返回元素有序的位置
+    return r;
+  }
+
+  function swap(i: number, j: number) {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
   }
 }
 // @lc code=end
+
+// function sortArray(nums: number[]): number[] {
+//   sort(0, nums.length - 1);
+//   return nums;
+
+//   function sort(left: number, right: number) {
+//     if (left === right) {
+//       // 单个元素不用排序
+//       return;
+//     }
+//     const mid = Math.floor((left + right) / 2);
+//     sort(left, mid);
+//     sort(mid + 1, right);
+//     merge(left, mid, right);
+//   }
+
+//   function merge(left: number, mid: number, right: number) {
+//     const temp = [];
+//     let l = left;
+//     let r = mid + 1;
+//     while (l < mid + 1 || r < right + 1) {
+//       if (l === mid + 1) {
+//         // 左侧数组已经全部合并，则将右边剩余数组直接拼接下来
+//         temp.push(nums[r]);
+//         r += 1;
+//       } else if (r === right + 1) {
+//         // 右侧数组已经全部合并，则将左边剩余数组直接拼接下来
+//         temp.push(nums[l]);
+//         l += 1;
+//       } else if (nums[l] > nums[r]) {
+//         // 左右指针两数相比取较小值，并前进指针
+//         temp.push(nums[r]);
+//         r += 1;
+//       } else {
+//         // temp[l] <= temp[r]
+//         temp.push(nums[l]);
+//         l += 1;
+//       }
+//     }
+//     // 原地更新数组
+//     for (let i = left; i <= right; i++) {
+//       nums[i] = temp[i - left];
+//     }
+//   }
+// }
 
 (() => {
   const nums = [5, 1, 1, 2, 0, 0];
