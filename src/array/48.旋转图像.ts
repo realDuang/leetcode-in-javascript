f0ff5f8a349bc62b01fc5f6285cb6c5c2281c41b
realDuang <250407778@qq.com>
2022-05-24 12:33:14 +0800

    feat: AC:48 并更新题解

---
+++ src/array/48.旋转图像.ts
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=typescript
+ *
+ * [48] 旋转图像
+ *
+ * https://leetcode.cn/problems/rotate-image/description/
+ *
+ * algorithms
+ * Medium (74.09%)
+ * Likes:    1307
+ * Dislikes: 0
+ * Total Accepted:    325.1K
+ * Total Submissions: 438.4K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ *
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[7,4,1],[8,5,2],[9,6,3]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+ * 输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == matrix.length == matrix[i].length
+ * 1 <= n <= 20
+ * -1000 <= matrix[i][j] <= 1000
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+  const len = matrix.length;
+  // 先将矩阵延左上到右下的对角线进行颠倒交换
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  // 之后将每一行的数据进行左右颠倒交换
+  for (let i = 0; i < len; i++) {
+    let left = 0;
+    let right = len - 1;
+    while (left < right) {
+      const temp = matrix[i][left];
+      matrix[i][left] = matrix[i][right];
+      matrix[i][right] = temp;
+      left += 1;
+      right -= 1;
+    }
+  }
+}
+// @lc code=end
+
+(() => {
+  const matrix = [
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16]
+  ];
+  console.log(rotate(matrix));
+})();
