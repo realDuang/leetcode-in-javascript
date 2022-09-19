960f9e8ccb45251715a4cd5f9f1f7e793bffbe6b
realduang <250407778@qq.com>
2022-09-19 15:31:56 +0800

    feat: AC 118,119

---
+++ src/array/118.杨辉三角.ts
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=typescript
+ *
+ * [118] 杨辉三角
+ *
+ * https://leetcode.cn/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (75.35%)
+ * Likes:    841
+ * Dislikes: 0
+ * Total Accepted:    345.8K
+ * Total Submissions: 459K
+ * Testcase Example:  '5'
+ *
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ * 
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: numRows = 5
+ * 输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: numRows = 1
+ * 输出: [[1]]
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 
+ * 
+ * 
+ */
+
+// @lc code=start
+function generate(numRows: number): number[][] {
+    const result: number[][] = [];
+    for (let i = 0; i < numRows; i++) {
+        const row = new Array(i + 1).fill(1);
+        for (let j = 1; j < row.length - 1; j++) {
+            row[j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+        result.push(row);
+    }
+    return result;
+};
+// @lc code=end
+
+(() => {
+    console.log(generate(5));
+})();
---
+++ src/array/119.杨辉三角Ii.ts
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=119 lang=typescript
+ *
+ * [119] 杨辉三角 II
+ *
+ * https://leetcode.cn/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (68.74%)
+ * Likes:    428
+ * Dislikes: 0
+ * Total Accepted:    228.5K
+ * Total Submissions: 332.4K
+ * Testcase Example:  '3'
+ *
+ * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+ * 
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: rowIndex = 3
+ * 输出: [1,3,3,1]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: rowIndex = 0
+ * 输出: [1]
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: rowIndex = 1
+ * 输出: [1,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 0 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你可以优化你的算法到 O(rowIndex) 空间复杂度吗？
+ * 
+ */
+
+// @lc code=start
+function getRow(rowIndex: number): number[] {
+    const result: number[] = Array(rowIndex + 1).fill(0);
+    result[0] = 1;
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i; j > 0; j--) {
+            result[j] += result[j - 1];
+        }
+    }
+    return result;
+};
+// @lc code=end
+
+(() => {
+    console.log(getRow(3))
+})();
